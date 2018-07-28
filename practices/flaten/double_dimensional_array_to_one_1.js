76725bed2741ed4e8ad340febfe74d1c477e069d
Liuming Yang <ylmap@126.com>
2018-07-28 21:45:36 +0800

    flaten has done

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -3,6 +3,19 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+  var result = [];
+
+  collection.forEach(element => {
+    if(element instanceof Array) {
+      element.forEach(ele => {
+        result.push(ele);
+      });
+    } else {
+      result.push(element);
+    }
+  });
+
+  return result;
 }
 
 module.exports = double_to_one;
--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -3,6 +3,24 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+  var arr = [];
+  let result = new Set();
+
+  collection.forEach(element => {
+    if(element instanceof Array) {
+      element.forEach(ele => {
+        arr.push(ele);
+      });
+    } else {
+      arr.push(element);
+    }
+  });
+
+  arr.forEach(element => {
+    result.add(element);
+  });
+
+  return Array.from(result);
 }
 
 module.exports = double_to_one;
